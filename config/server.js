28b9ba8ba3fc878365176d8d33fb244a0525a0a1
vuduccuong <vuduccuong.ck48@gmail.com>
2021-05-12 23:26:21 +0700

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a3f3a785275ba6e184fc6dad27f611c7'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd3e4fdd9-b2a0-4d7c-aae9-5686fb894d06'
+};
